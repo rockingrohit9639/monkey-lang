fafdadf38003770d34403f8c5d00aaffcd66bcb4
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2025-02-15 16:00:13 +0100

    feat(eval): evaluating prefix expressions

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -15,6 +15,10 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"5", 5},
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
@@ -38,6 +42,28 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}
 }
 
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+/*
+Helper functions
+*/
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
