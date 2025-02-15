4d53e9a3d76204d3482708d5855224dd141ad78d
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2025-02-15 21:33:39 +0100

    feat: add support for strings

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,6 +21,8 @@ return false;
 }
 10 == 10;
 10 != 9;
+"foobar"
+"foo bar"
 `
 
 	tests := []struct {
@@ -99,6 +101,8 @@ return false;
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
